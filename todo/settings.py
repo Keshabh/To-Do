51e8a0171a411c99c11c189997c53e814a9441a7
keshabh78 <keshabh_201900185@smit.smu.edu.in>
2021-12-21 00:47:48 +0530

    Deploy

--- todo/settings.py
+++ todo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o0+cxng-+_#n!krvfn&!=%gax$p(rgas)-n*x@(51i33*-6cn^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','taskschecklist.herokuapp.com']
+ALLOWED_HOSTS = ['taskschecklist.herokuapp.com',"localhost"]
 
 
 # Application definition
